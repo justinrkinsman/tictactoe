c0132f390a937f674a57d4e952ee616ad2131e24
Justin <justinrkinsman@gmail.com>
2022-06-07 12:30:42 -0300

    Associate DOM grid with array

--- javascript.js
+++ javascript.js
@@ -5,28 +5,33 @@ function sayHello(){
     console.log('Hello')
 }*/
 
-let grid = ['', '', '', '', '', '', '', '', ''];
+//let grid = ['', '', '', '', '', '', '', '', ''];
 
 document.addEventListener('click', function(e){
+    gameBoard
     if(e.target && e.target.textContent == ''){
         e.target.value = 'X'
-        grid.splice(((e.target.id.slice(-1))-1), 1, 'X')
-        console.log(grid)
+        let newGrid = gameBoard.grid
+        newGrid.splice(((e.target.id.slice(-1))-1), 1, 'X')
+        console.log(newGrid)
        // randomO
+       playerTwo()
     }
     //const randomO = grid[Math.floor(Math.random() * grid.length)];
 })
 
 const gameBoard = (() => {
-    const cell1 = document.getElementById('cell1');
-    const cell2 = document.getElementById('cell2');
-    const cell3 = document.getElementById('cell3');
-    const cell4 = document.getElementById('cell4');
-    const cell5 = document.getElementById('cell5');
-    const cell6 = document.getElementById('cell6');
-    const cell7 = document.getElementById('cell7');
-    const cell8 = document.getElementById('cell8');
-    const cell9 = document.getElementById('cell9');
+    let grid = []
+    grid.splice(0, 1, (document.getElementById('cell1')));
+    grid.splice(1, 1, (document.getElementById('cell2')));
+    grid.splice(2, 1, (document.getElementById('cell3')));
+    grid.splice(3, 1, (document.getElementById('cell4')));
+    grid.splice(4, 1, (document.getElementById('cell5')));
+    grid.splice(5, 1, (document.getElementById('cell6')));
+    grid.splice(6, 1, (document.getElementById('cell7')));
+    grid.splice(7, 1, (document.getElementById('cell8')));
+    grid.splice(8, 1, (document.getElementById('cell9')));
+    return {grid}
 })()
 
 const player = (name, selection) => {
@@ -36,3 +41,11 @@ const player = (name, selection) => {
 
 const playerOne = player('justin', 'x')
 playerOne.sayHello()
+
+function playerTwo() {
+    const randomO = grid[Math.floor(Math.random() * grid.length)]
+    if (randomO == ''){
+       // randomO.textContent = 'O'
+        console.log('O')
+    }
+}
